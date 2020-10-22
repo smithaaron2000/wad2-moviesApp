891b7922f0728fab9cc65968c60b7afeb445af9d
Aaron Tierney Smith <smithaaron2000@gmail.com>
2020-10-22 18:32:10 +0100

    Movie Details page components

--- stories/index.js
+++ stories/index.js
@@ -5,8 +5,8 @@ import MovieCard from "../src/components/movieCard";
 import FilterControls from "../src/components/filterControls";
 import MoviesHeader from "../src/components/headerMovieList";
 import MovieList from "../src/components/movieList";
-
-
+import MovieDetails from "../src/components/movieDetails";
+import MovieHeader from '../src/components/headerMovie'
 
 
 const sample = {
@@ -110,3 +110,10 @@ storiesOf("Home Page/MovieCard", module)
     return <MovieList movies={movies} />
 });
 
+storiesOf("Movie Details Page/MovieDetails", module).add("default", () => (
+  <MovieDetails movie={sample} />
+));
+
+storiesOf("Movie Details Page/MovieHeader", module).add("default", () => (
+  <MovieHeader movie={sample} />
+));
