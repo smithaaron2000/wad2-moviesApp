072e49106f9b969840345fbc0e2894847ef145bc
Aaron Tierney Smith <smithaaron2000@gmail.com>
2020-10-23 21:27:17 +0100

    Added routing and live data from API

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -2,20 +2,22 @@ import React from "react";
 import "./movieCard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../globals/fontawesome";
-
+import { Link } from "react-router-dom";
 const MovieCard = props => {
   return (
     <div className="col-sm-3">
       <div className="card  bg-white">
-        <img
-          className="card-img-tag center "
-          alt={props.movie.title}
-          src={
-            props.movie.poster_path
-              ? `https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`
-              : "./film-poster-placeholder.png"
-          }
-        />
+      <Link to={`/movies/${props.movie.id}`}>
+    <img
+      className="card-img-tag center "
+      alt={props.movie.title}
+      src={
+        props.movie.poster_path
+          ? `https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`
+          : "./film-poster-placeholder.png"
+      }
+    />
+  </Link>
         <div className="card-body">
           <h4 className="card-title ">{props.movie.title}</h4>
           <p>
